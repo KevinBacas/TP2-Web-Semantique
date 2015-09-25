151c431fede836269906d5af787ef73da1c682ed
KevinBacas <bacas.kevin@hotmail.fr>
2015-09-25 14:09:29 -0700

    Recompiled prod

--- dist/Requeter.js
+++ dist/Requeter.js
@@ -1,12 +1,18 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
 	value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _immutable = require('immutable');
+
+var _immutable2 = _interopRequireDefault(_immutable);
 
 var Requeter = (function () {
 	function Requeter(request, indexer) {
@@ -20,7 +26,7 @@ var Requeter = (function () {
 	}
 
 	_createClass(Requeter, [{
-		key: "calculCoefficient",
+		key: 'calculCoefficient',
 		value: function calculCoefficient() {
 			// C(v,r) = Somme (pondération des mot) / Racine(Somme (pondération des mot)²
 			var docTable = this.indexer.getDocumentArray();
@@ -32,25 +38,33 @@ var Requeter = (function () {
 					var doc_name = docTable[doc];
 					numerateur = this.indexer.ponderationMot(this.requestArray, doc_name);
 					denominateur = this.indexer.ponderationMotCarre(this.requestArray, doc_name);
-					res.push(numerateur / denominateur);
+					res.push(isNaN(numerateur / denominateur) ? 0 : numerateur / denominateur);
 				}
 			}
 			return res;
 		}
 	}, {
-		key: "resultatRequest",
+		key: 'resultatRequest',
 		value: function resultatRequest() {
-			//TODO: Renvoyer la liste des documents par ordre de pondération
 			var results = this.calculCoefficient();
 			var documents = this.indexer.getDocumentArray();
-			console.log("results", results);
-			console.log("documents", documents);
-			//TODO: Renvoyer la pondération avec la liste des documents
+			var res = new _immutable2['default'].OrderedMap();
+			console.log(res);
+			for (var i = 0; i < results.length; ++i) {
+				res = res.set(results[i], {
+					ponderation: results[i],
+					document: documents[i]
+				});
+			}
+			res = res.sortBy(function (doc) {
+				return doc.ponderation;
+			});
+			return res.toJS();
 		}
 	}]);
 
 	return Requeter;
 })();
 
-exports["default"] = Requeter;
-module.exports = exports["default"];
+exports['default'] = Requeter;
+module.exports = exports['default'];
